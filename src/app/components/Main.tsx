b3355a5b67daffd947248312414cd82a56ec8783
Dekel Zoaretz <dekelz@voyagerlabs.co>
2021-12-14 09:36:01 +0000

    adding box shadow on hover

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -22,6 +22,19 @@ const ImageWrapper = styled.img`
   object-fit: contain;
   height: 300px;
   width: 300px;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 2.1px 0 12.9px -4px rgb(0 0 0 / 3%), 7.1px 0px 43.3px -4px rgb(0 0 0 / 4%), 32px 0px 194px -4px rgb(0 0 0 / 7%);
+  
+  &:hover {
+    border: 1px solid #d7d7d7;
+    box-shadow:
+            2.5px 0 175.5px rgba(0, 0, 0, 0.114),
+            8.5px 0 180.9px rgba(0, 0, 0, 0.127),
+            38px 0 365px rgba(0, 0, 0, 0.26)
+  ;
+
+  }
 `;
 
 interface MainProps {
